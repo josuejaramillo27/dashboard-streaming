c8a15f8c260e7b5b1385604833f00f168838f2d7
Josué Jaramillo Michilot <jaramillojosue2006@gmail.com>
2026-06-02 03:10:14 -0500

    Update app.js

--- app.js
+++ app.js
@@ -273,7 +273,8 @@ window.openProfileModal = () => {
     document.getElementById('editProfileCountry').value = currentUserData.country || ''; 
     document.getElementById('editProfilePhone').value = currentUserData.phone || ''; 
     document.getElementById('editProfileAlias').value = currentUserData.storeAlias || ''; 
-    document.getElementById('profileModal').style.display = 'flex'; 
+    document.getElementById('profileModal').style.display = 'flex';
+    document.getElementById('editProfileExternalStore').value = currentUserData.externalStoreUrl || '';
 };
 
 window.saveProfile = async () => { 
@@ -299,12 +300,16 @@ window.saveProfile = async () => {
         let rawAlias = document.getElementById('editProfileAlias').value;
         let finalAlias = rawAlias.trim().toLowerCase().replace(/\s+/g, '-').replace(/[^a-z0-9-]/g, '');
 
+        const externalStore = document.getElementById('editProfileExternalStore').value.trim();
+
         await updateDoc(doc(db, "users", currentUser.uid), { 
             name: name, country: country, currency: getCurrencyForCountry(country), 
-            phone: phone, logoUrl: logoUrl, storeAlias: finalAlias 
+            phone: phone, logoUrl: logoUrl, storeAlias: finalAlias,
+            externalStoreUrl: externalStore // <-- NUEVO CAMPO
         }); 
         
-        currentUserData.storeAlias = finalAlias; // Actualizamos en memoria 
+        currentUserData.storeAlias = finalAlias;
+        currentUserData.externalStoreUrl = externalStore; // <-- ACTUALIZA MEMORIA
         
         currentUserData.name = name;
         currentUserData.country = country;
